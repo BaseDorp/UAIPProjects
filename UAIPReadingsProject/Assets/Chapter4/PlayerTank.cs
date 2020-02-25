73823c44c72f941b13d95ae9e495ed5179de4e8d
BaseDorp <44095612+BaseDorp@users.noreply.github.com>
2020-02-25 00:44:48 -0600

    Chapter 4 Done

---
+++ UAIPReadingsProject/Assets/Chapter4/PlayerTank.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTank : MonoBehaviour
+{
+    public Transform targetTransform;
+    private float movementSpeed, rotSpeed;
+
+    private void Start()
+    {
+        movementSpeed = 10.0f;
+        rotSpeed = 2.0f;
+    }
+
+    private void Update()
+    {
+        if (Vector3.Distance(transform.position, targetTransform.position) < 5.0f)
+        {
+            return;
+        }
+
+        Vector3 targetPos = targetTransform.position;
+        targetPos.y = transform.position.y;
+        Vector3 dirRot = targetPos - transform.position;
+
+        Quaternion tarRot = Quaternion.LookRotation(dirRot);
+        transform.rotation = Quaternion.Slerp(transform.rotation, tarRot, rotSpeed * Time.deltaTime);
+
+        transform.Translate(new Vector3(0, 0, movementSpeed * Time.deltaTime));
+
+    }
+}
---
+++ UAIPReadingsProject/Assets/Chapter4/Target.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    int button = 0;
+
+    void Update()
+    {
+        button = 0;
+
+        if (Input.GetMouseButtonDown(button))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(ray.origin, ray.direction, out hitInfo))
+            {
+                Vector3 targetPosition = hitInfo.point;
+                transform.position = targetPosition;
+            }
+        }
+    }
+}
