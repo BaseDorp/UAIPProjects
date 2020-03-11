d045feb4dac7cbda562ac809c492769181281d5a
BaseDorp <44095612+BaseDorp@users.noreply.github.com>
2020-03-11 00:57:46 -0500

    Chapter 6 Done

---
+++ UAIPReadingsProject/Assets/Chapter6/Avoidance.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Avoidance : MonoBehaviour
+{
+    public Path path;
+    public float speed = 20.0f;
+    public float mass = 5.0f;
+    public bool isLooping = true;
+ 
+    private float curSpeed;
+
+    private int curPathIndex;
+    private float pathLength;
+    private Vector3 targetPoint;
+
+    Vector3 velocity;
+    
+
+    void Start()
+    {
+        pathLength = path.Length;
+        curPathIndex = 0;
+
+        // get the current velocity of the vehicle
+        velocity = transform.forward;
+    }
+    
+
+    void Update()
+    {
+        curSpeed = speed * Time.deltaTime;
+
+        targetPoint = path.GetPoint(curPathIndex);
+
+        // Moves to next spot on path
+        if (Vector3.Distance(transform.position, targetPoint) < path.Radius)
+        {
+            //Don't move the vehicle if path is finished 
+            if (curPathIndex < pathLength - 1)
+                curPathIndex++;
+            else if (isLooping)
+                curPathIndex = 0;
+            else
+                return;
+        }
+
+        // move until agent is at the end of the path
+        if (curPathIndex >= pathLength)
+            return;
+
+        // Velocity towards next point
+        if (curPathIndex >= pathLength - 1 && !isLooping)
+            velocity += Steer(targetPoint, true);
+        else
+            velocity += Steer(targetPoint);
+
+        transform.position += velocity;
+        transform.rotation = Quaternion.LookRotation(velocity);
+    }
+
+    // Steers the agent to the next point
+    public Vector3 Steer(Vector3 target, bool bFinalPoint = false)
+    {
+        //Calculate the directional vector from the current position towards the target point
+        Vector3 desiredVelocity = (target - transform.position);
+        float dist = desiredVelocity.magnitude;
+
+        //Normalise the desired Velocity
+        desiredVelocity.Normalize();
+
+        //Calculate the velocity according to the speed
+        if (bFinalPoint && dist < 10.0f)
+            desiredVelocity *= (curSpeed * (dist / 10.0f));
+        else
+            desiredVelocity *= curSpeed;
+
+        Vector3 steeringForce = desiredVelocity - velocity;
+        Vector3 acceleration = steeringForce / mass;
+
+        return acceleration;
+    }
+}
